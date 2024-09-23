94244f53c1f00988e24870683148864678d97180
RommJames <rj.cuya010@gmail.com>
2024-09-23 19:16:44 +0800

    Finished 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let result = ""
+    if(num < 0){
+        result = "ERROR"
+    }
+    for(let ctr = 1; ctr <= num; ctr++){
 
+        result += text
+    }
+    return result
 };
 
 // Do not edit below this line
