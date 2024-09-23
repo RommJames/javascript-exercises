cf3909fb28ba114a8536ae11895a5a2fab111f0b
RommJames <rj.cuya010@gmail.com>
2024-09-23 20:36:51 +0800

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let result = [...new Set(array)] 
+    for(removeValue of remove){
+        if(result.includes(removeValue)){
+            let removeIt = result.indexOf(removeValue)
+            result.splice(removeIt,1)            
+        }
+        
+    }
+    
+
+    return result    
 };
+
+console.log(removeFromArray([1,2,3,4],1, 2 , 3, 4))
 
 // Do not edit below this line
 module.exports = removeFromArray;
