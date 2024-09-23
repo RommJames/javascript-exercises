f8054e8d96cc59d6f25e3e8fa2ccd62846621520
RommJames <rj.cuya010@gmail.com>
2024-09-23 19:29:04 +0800

    03 Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let result = []
+    let wordArray = word.split("")    
+    
+    for(let ctr = wordArray.length - 1; ctr >= 0;ctr--){
 
+        result.push(wordArray[ctr])        
+    }
+    
+
+    return result.join("")
 };
+
+console.log(reverseString("sample"))
 
 // Do not edit below this line
 module.exports = reverseString;
