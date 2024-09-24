f467b7ec87c2bcef7eac7e6c9d1ecf2640262152
RommJames <rj.cuya010@gmail.com>
2024-09-24 18:21:21 +0800

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum = 0
+    let holder
+    let lessZero = first < 0 || last < 0
+    let checkInt = !Number.isInteger(first) || !Number.isInteger(last) || Array.isArray(first) || Array.isArray(last)
 
+    if(last < first){
+        holder = first
+        first = last
+        last = holder
+    }    
+    if(lessZero || checkInt){        
+        console.log(checkInt)
+        return "ERROR"
+    }
+
+    for(let ctr = first; ctr <= last; ctr++){
+        sum += ctr;
+    }
+
+    return sum
 };
+
+console.log(sumAll(123, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
