95e4d9a2084b5e5b2695780f80828e893e787cb0
RommJames <rj.cuya010@gmail.com>
2024-09-24 18:50:13 +0800

    06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear
+    if(year % 4 == 0 && year % 100 != 0) leapYear = true;            
+    else if(year % 100 == 0 && year % 400 == 0) leapYear = true
+    else leapYear = false;
 
+    return leapYear
 };
-
+console.log(leapYears(34992))
 // Do not edit below this line
 module.exports = leapYears;
