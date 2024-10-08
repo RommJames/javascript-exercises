fa7323814a948b4fc5d79b2a5cd6ccbe91d8639d
RommJames <rj.cuya010@gmail.com>
2024-10-08 10:53:19 +0800

    10_fibonacci, Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let first = 0
+    let second = 1
+    let total = num == 1 ? 1 : 0;
+    for (let i = 1; i < num; i++) {                
+        total = first + second
+        first = second
+        second = total
+    }
+    console.log(total);
+    return num < 0 ? "OOPS" : total;
 };
+console.log(fibonacci(1));
 
 // Do not edit below this line
 module.exports = fibonacci;
