89326664fb0eea649594aa3663f80307d9673272
RommJames <rj.cuya010@gmail.com>
2024-10-08 13:17:19 +0800

    12_findTheOldest, Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,53 @@
-const findTheOldest = function() {
+// const people = [
+//     {
+//       name: 'Carly',
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: 'Ray',
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: 'Jane',
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ];
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+function getAge(yearOfBirth, yearOfDeath){
+
+    if(!yearOfDeath){
+        yearOfDeath = new Date().getFullYear()
+    }        
+    return yearOfDeath - yearOfBirth
+}
+
+const findTheOldest = function(arr) {    
+   let list = arr.map((person)=> [person.name, getAge(person.yearOfBirth, person.yearOfDeath)]).sort((a, b)=> a[1] > b[1] ? -1 : 1);
+//    "a: ", a, "-- b: ", b
+   console.log(list);
+   let oldest = arr.find((person)=> person.name === list[0][0]);   
+   console.log(oldest);
+   return oldest;
 };
-
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
