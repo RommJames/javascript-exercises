7e191827a4a0d47719fd43c10f4e68dec9be8411
RommJames <rj.cuya010@gmail.com>
2024-10-08 10:22:56 +0800

    09_palindromes, Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let validChar = "abcdefghijklmnopqrstuvwxyz1234567890".split("");
+    let splitWord = word.toLowerCase().split("");
+    let checkValidChar = splitWord.filter((char) => validChar.includes(char));
+    let origWord = checkValidChar.join("");
+    let reverse = checkValidChar.reverse().join("");
+    
+    console.log(origWord, "------", reverse);
+    return origWord === reverse;
+    console.log(checkValidChar);
 };
+console.log(palindromes("r3ace3car"));
 
 // Do not edit below this line
 module.exports = palindromes;
